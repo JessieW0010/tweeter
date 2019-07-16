402334d9379387d1105be4f7261a9d92b70ea28e
Jessie Wang <wangj.5415@gmail.com>
2019-07-16 21:09:42 +0000

    finished final styling on tweet-container.css

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,35 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+/* 
+<article>
+          <header class="tweet">
+            <span>
+                <img src="images/profile-hex.png">
+                <h3>Superlongname</h3>
+            </span>
+            <h3 id="handle">@Handle</h3>
+          </header>
+          <p class="tweet-input"> Tweet </p>
+          <footer>
+            <h6 class="tweet days-ago">x days ago</h6>
+            <div class="tweet-icons">
+              <a><img src="https://image.flaticon.com/icons/svg/149/149804.svg" alt="flag"></a>
+              <a><img src="https://image.flaticon.com/icons/svg/3/3890.svg" alt="retweet"></a>
+              <a><img src="https://image.flaticon.com/icons/svg/4/4458.svg" alt="like"></a>
+            </div>
+          </footer>
+   </article> 
+*/
+
+// function takes a tweet object and returns a tweet <article> element containing the entire HTMl structure of the tweet
+const createTweetElement = function(obj) {
+  const $tweet = $("<article>").append(
+    $(`<header class="tweet">`).append(
+      $(`<span>`).append(
+        $(`<img src="images/profile-hex.png">
+        <h3>Superlongname</h3>`)
+      )
+    )
+  )
+}
