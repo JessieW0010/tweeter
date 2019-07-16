c71da96a080bb02398130526ef78d162130ed4bb
Jessie Wang <wangj.5415@gmail.com>
2019-07-16 22:05:57 +0000

    added to app.js in scripts/, createTweetElement takes in a tweet object and returns the appropriate HTML to append to the tweets-container div, renderTweets takes in an array of tweet objects and apends their HTML

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,35 +4,75 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-/* 
-<article>
-          <header class="tweet">
-            <span>
-                <img src="images/profile-hex.png">
-                <h3>Superlongname</h3>
-            </span>
-            <h3 id="handle">@Handle</h3>
-          </header>
-          <p class="tweet-input"> Tweet </p>
-          <footer>
-            <h6 class="tweet days-ago">x days ago</h6>
-            <div class="tweet-icons">
-              <a><img src="https://image.flaticon.com/icons/svg/149/149804.svg" alt="flag"></a>
-              <a><img src="https://image.flaticon.com/icons/svg/3/3890.svg" alt="retweet"></a>
-              <a><img src="https://image.flaticon.com/icons/svg/4/4458.svg" alt="like"></a>
-            </div>
-          </footer>
-   </article> 
-*/
+$(document).ready(function() {
 
 // function takes a tweet object and returns a tweet <article> element containing the entire HTMl structure of the tweet
 const createTweetElement = function(obj) {
-  const $tweet = $("<article>").append(
-    $(`<header class="tweet">`).append(
-      $(`<span>`).append(
-        $(`<img src="images/profile-hex.png">
-        <h3>Superlongname</h3>`)
-      )
-    )
-  )
+
+  const $header = `
+      <header class="tweet">
+        <span class="tweet-avatar">
+          <img src=${obj.user["avatars"]}>
+          <p class="tweet-name profile bold">${obj.user["name"]}</p>
+        </span>
+        <p class="tweet-handle profile bold">${obj.user["handle"]}</p>
+      </header>`
+  const $p = `<p class="tweet-input bold">${obj.content["text"]}</p>`;
+  const $footer = `
+      <footer>
+        <p class="tweet-days bold">${obj.created_at}</p>
+        <div class="tweet-icons">
+          <a><img src="https://image.flaticon.com/icons/svg/149/149804.svg" alt="flag"></a>
+          <a><img src="https://image.flaticon.com/icons/svg/3/3890.svg" alt="retweet"></a>
+          <a><img src="https://image.flaticon.com/icons/svg/4/4458.svg" alt="like"></a>
+        </div>
+      </footer>`;
+
+  return `
+    <article>
+      ${$header}
+      ${$p}
+      ${$footer}
+    </article>`
+
 }
+
+const renderTweets = function(tweets) {
+  // loops through tweets
+  // calls createTweetElement for each tweet
+  // takes return value and appends it to the tweets container
+
+  for (let tweet of tweets) {
+    console.log(createTweetElement(tweet));
+    $('#tweets-container').append(createTweetElement(tweet));
+  }
+}
+
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+renderTweets(data);
+
+});
