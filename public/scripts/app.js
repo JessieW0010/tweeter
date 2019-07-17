32c3b61ddb4973c3acf88803b74e297549371388
Jessie Wang <wangj.5415@gmail.com>
2019-07-17 17:20:19 +0000

    used ajax to reroute our submit tweet form to the same page and submit our tweets to the tweet databse

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,7 +5,6 @@
  */
 
 $(document).ready(function() {
-
 // function takes a tweet object and returns a tweet <article> element containing the entire HTMl structure of the tweet
 const createTweetElement = function(obj) {
 
@@ -48,31 +47,22 @@ const renderTweets = function(tweets) {
   }
 }
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+const loadTweets = function() {
+  $.ajax("/tweets", {
+    method: "GET"
+  })
+}
 
-renderTweets(data);
+$("#tweet-text").on("submit", function(event) {
+  event.preventDefault();
+  const data = $(this).serialize();
+  $.ajax("/tweets", {
+    method: "POST", 
+    data, 
+    success: function() {
+      console.log(data);
+    }
+  })
+})
 
-});
+})
